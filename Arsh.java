19a1a42cc22b8cfc229f6b9ba1aef7ef3fb2c6d2
Raghav140 <raghavsharma.jan2004@gmail.com>
2024-08-30 18:25:45 +0530

    Scanner

---
+++ Arsh.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Arsh{
+    public static void main(String args[]){
+    Scanner obj=new Scanner(System.in);
+    System.out.println("Enter Your Name :");
+
+ int name=obj.nextInt();
+ System.out.println("The name is :"+name); 
+
+    }
+}
+
